8a72dc9e5a9b7bdd612cfbc89013282602e117a0
crymimi3 <ggmod8787@gmail.com>
2015-04-27 19:57:08 +0800

    cry3

---
+++ crymimi3.java
@@ -0,0 +1,14 @@
+﻿import java.util.*;
+class crymimi3{
+ 
+	public static void main(String[]args){
+
+		Scanner input=new Scanner(System.in);		
+		int u=input.nextInt();
+		for(int i=0;i<u;i++){
+			System.out.println(i);
+		}
+		System.out.println("ggmod");
+
+	}
+}
